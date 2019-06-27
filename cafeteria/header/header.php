b5c0a8b3cf079fbfc1de41c4d06d118dcbb8d8e7
GuilhermeSSilva <guilherme_rgcosta@hotmail.com>
2019-06-27 15:07:39 -0300

    header

--- cafeteria/header/header.php
+++ cafeteria/header/header.php
@@ -1,13 +1,17 @@
 <header id="header">
-		<div class="main">
-			<script type="text/javascript">
+		<div id="main" class="main">
+			<?php
 				session_start(); 
-				var user = <?php echo $_SESSION['id_type_user']; ?>
+			?>
+			<script type="text/javascript">
+				
+				var user = "<?php echo $_SESSION['id_type_user']; ?>";
+				var name = "<?php echo $_SESSION['name']; ?>";
 
 				if (user == 1) {
-					 document.getElementById("main").innerHTML = "<h1><a href='../index/index.html'>Coffee<span>Time</span></a></h1><ul><li><a href='../search/search.html'>Pesquisa</a></li></li><li><a href='../cadastro_produtos/cadastro.php'>Cadastro</a></li><li><a href='nome_produto.php'>Nome produtos </a></li><li>'<?php echo $_SESSION['name'];?>'</li><li><a>Logout</a></li></ul>";
+					 document.getElementById("main").innerHTML = "<h1><a href='../index/index.html'>Coffee<span>Time</span></a></h1><ul><li><a href='../search/search.html'>Pesquisa</a></li></li><li><a href='../cadastro_produtos/cadastro.php'>Cadastro</a></li><li><a href='nome_produto.php'>Nome produtos </a></li><li>"+name+"</li><li><a>Logout</a></li></ul>";
 				}else{
-					document.getElementById("main").innerHTML = "<h1><a href='../index/index.html'>Coffee<span>Time</span></a></h1><ul><li><a href='../search/search.html'>Pesquisa</a></li></li><li>'<?php echo $_SESSION['name'];?>'</li><li><a>Logout</a></li></ul>";
+					document.getElementById("main").innerHTML = "<h1><a href='../index/index.html'>Coffee<span>Time</span></a></h1><ul><li><a href='../search/search.html'>Pesquisa</a></li></li><li>"+name+"</li><li><a>Logout</a></li></ul>";
 				}
 			</script>	
 	</header>

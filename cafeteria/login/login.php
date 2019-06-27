9e184c25096be8717ea9e539a3b6ca878b8d3549
Michel de Quadros Krüger <micheldequadros@hotmail.com>
2019-06-27 09:44:59 -0300

    Update login.php
    test

--- cafeteria/login/login.php
+++ cafeteria/login/login.php
@@ -11,7 +11,6 @@
 		<input type="text" name="" class="input" placeholder="Escreva login">
 		<input type="number" name="" class="input" placeholder="Escreva sua senha">
 		<input type="submit" name="Enviar" value="Logar" class="enviar">
-		<label>Esqueceu a <a href="">conta</a>?</label>
 		<label>Não tem uma conta? <a href="../cadastro_usuario/cadastro_usuario.php">Cadastre-se.</a></label>
 	</form>
 
